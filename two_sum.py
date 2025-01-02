25ffb5ea734c0e6eb68af965d5c0818cf7701704
Shawn Hyder <sxh5603@mavs.uta.edu>
2025-01-01 19:23:49 -0600

    Implemended two sum

---
+++ two_sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        myDict = {}
+        ind = []
+
+        for i in range(len(nums)):
+            t = target - nums[i]
+            if(t in myDict):
+                ind.append(myDict[t])
+                ind.append(i)
+                return ind
+            else:
+                myDict[nums[i]] = i
+            
